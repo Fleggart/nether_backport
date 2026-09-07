3bd480b265c65ff2dac70248b64e1bd91b59fedb
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-07 17:03:49 +0800

    Add files via upload

--- src/main/java/com/unseen/nb/proxy/CommonProxy.java
+++ src/main/java/com/unseen/nb/proxy/CommonProxy.java
@@ -1,5 +1,6 @@
 package com.unseen.nb.proxy;
 
+import com.unseen.nb.common.event.EventOnSoulFire;
 import com.unseen.nb.common.network.ParticleMessage;
 import com.unseen.nb.init.ModNetworkPackets;
 import net.minecraft.item.Item;
@@ -10,7 +11,9 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 
 public class CommonProxy {
 
-
+    public void init() {
+        MinecraftForge.EVENT_BUS.register(new EventOnSoulFire());
+    }
 
 
 
