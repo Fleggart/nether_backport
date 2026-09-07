d83201ab41bd02218657086bea2b08748e9bad41
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-07 17:03:08 +0800

    Add files via upload

--- src/main/java/com/unseen/nb/util/ModUtils.java
+++ src/main/java/com/unseen/nb/util/ModUtils.java
@@ -72,7 +72,11 @@ public class ModUtils {
             }
         }
 
-        
+        for(BlockPos blockPos : affectedConversionPositions) {
+            if(world.getBlockState(blockPos).getBlock() == ModBlocks.SOUL_SOIL) {
+                world.setBlockState(blockPos, Blocks.SOUL_SAND.getDefaultState());
+            }
+        }
     }
 
 
