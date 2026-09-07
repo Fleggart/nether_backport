89be183a955348adac82a494665911cfb0104526
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-07 18:03:50 +0800

    Remove netherite horse armor items and imports
    Removed unused netherite horse armor references and imports.

--- src/main/java/com/unseen/nb/init/ModItems.java
+++ src/main/java/com/unseen/nb/init/ModItems.java
@@ -1,7 +1,7 @@
 package com.unseen.nb.init;
 
 import com.unseen.nb.common.items.*;
-import com.unseen.nb.common.items.netherite.*;
+// 删除 import com.unseen.nb.common.items.netherite.*;
 import com.unseen.nb.util.ModReference;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.passive.HorseArmorType;
@@ -19,14 +19,13 @@ import java.util.List;
 public class ModItems {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
+    // 删除这行
+    // public static final HorseArmorType NETHERITE_HORSE_ARMOR = EnumHelper.addHorseArmor(...);
 
-    public static final HorseArmorType NETHERITE_HORSE_ARMOR = EnumHelper.addHorseArmor("netheriteHorseArmor", ModReference.MOD_ID + ":textures/models/armor/horse/horse_armor_netherite.png", 14);
     public static final Item LODE_STONE_COMPASS = new ItemLodeStoneCompass("lode_stone_compass");
 
-    public static final Item NETHERITE_HORSE_ARMOR_ITEM = new ItemNBHorseArmor("netherite_horse_armor", NETHERITE_HORSE_ARMOR, CreativeTabs.MISC);
-    //Keeping Pigstep cause it has no generation rules in FutureMC
+    // 删除这行
+    // public static final Item NETHERITE_HORSE_ARMOR_ITEM = new ItemNBHorseArmor(...);
 
     public static final Item PIGSTEP_MUSIC_DISC = new ItemPigStepDisc("pig_step_disc", ModSoundHandler.PIG_STEP_DISC);
-
-
 }
