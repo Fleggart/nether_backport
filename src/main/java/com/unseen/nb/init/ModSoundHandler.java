ccd676b492e9db97e97eb573b9eed614c83c0ea2
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-09 00:48:11 +0800

    Update ModSoundHandler.java

--- src/main/java/com/unseen/nb/init/ModSoundHandler.java
+++ src/main/java/com/unseen/nb/init/ModSoundHandler.java
@@ -7,30 +7,36 @@ import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class ModSoundHandler {
 
-    // ===== NBSoundTypes 需要的音效 =====
-    
+    // ===== 锁链音效 =====
+    public static SoundEvent CHAIN_BREAK;
+    public static SoundEvent CHAIN_STEP;
+
+    // ===== 下界金矿石音效 =====
     public static SoundEvent NETHER_ORE_BREAK;
     public static SoundEvent NETHER_ORE_STEP;
     public static SoundEvent NETHER_ORE_PLACE;
     public static SoundEvent NETHER_ORE_BLOCK_HIT;
 
+    // ===== 菌光体音效 =====
     public static SoundEvent SHROOM_LIGHT_BREAK;
     public static SoundEvent SHROOM_LIGHT_STEP;
     public static SoundEvent SHROOM_LIGHT_PlACE;
     public static SoundEvent SHROOM_LIGHT_HIT;
 
+    // ===== 灵魂灯笼音效 =====
     public static SoundEvent SOUL_LANTERN_BREAK;
     public static SoundEvent SOUL_LANTERN_PlACE;
 
+    // ===== 下界合金音效 =====
     public static SoundEvent NETHERITE_BREAK;
     public static SoundEvent NETHERITE_STEP;
     public static SoundEvent NETHERITE_PLACE;
     public static SoundEvent NETHERITE_BLOCK_HIT;
 
+    // ===== 远古残骸音效 =====
     public static SoundEvent ANCIENT_DEBRIS_BREAK;
 
     // ===== 其他音效 =====
-    
     public static SoundEvent SOUL_FIRE_AMBIENT;
 
     // Biome Ambience
@@ -48,30 +54,36 @@ public class ModSoundHandler {
     public static SoundEvent NETHERITE_EQUIP;
 
     public static void registerSounds() {
-        // ===== 注册 NBSoundTypes 需要的音效 =====
-        
+        // ===== 锁链音效 =====
+        CHAIN_BREAK = registerSound("chain.break", "block");
+        CHAIN_STEP = registerSound("chain.step", "block");
+
+        // ===== 下界金矿石音效 =====
         NETHER_ORE_BREAK = registerSound("nether_ore.break", "block");
         NETHER_ORE_STEP = registerSound("nether_ore.step", "block");
         NETHER_ORE_PLACE = registerSound("nether_ore.place", "block");
         NETHER_ORE_BLOCK_HIT = registerSound("nether_ore.hit", "block");
 
+        // ===== 菌光体音效 =====
         SHROOM_LIGHT_BREAK = registerSound("shroom_light.break", "block");
         SHROOM_LIGHT_STEP = registerSound("shroom_light.step", "block");
         SHROOM_LIGHT_PlACE = registerSound("shroom_light.place", "block");
         SHROOM_LIGHT_HIT = registerSound("shroom_light.hit", "block");
 
+        // ===== 灵魂灯笼音效 =====
         SOUL_LANTERN_BREAK = registerSound("soul_lantern.break", "block");
         SOUL_LANTERN_PlACE = registerSound("soul_lantern.place", "block");
 
+        // ===== 下界合金音效 =====
         NETHERITE_BREAK = registerSound("netherite.break", "block");
         NETHERITE_STEP = registerSound("netherite.step", "block");
         NETHERITE_PLACE = registerSound("netherite.place", "block");
         NETHERITE_BLOCK_HIT = registerSound("netherite.hit", "block");
 
+        // ===== 远古残骸音效 =====
         ANCIENT_DEBRIS_BREAK = registerSound("debris.break", "block");
 
-        // ===== 注册其他音效 =====
-        
+        // ===== 其他音效 =====
         SOUL_FIRE_AMBIENT = registerSound("soul_fire.ambient", "block");
         
         // Biome Ambience
