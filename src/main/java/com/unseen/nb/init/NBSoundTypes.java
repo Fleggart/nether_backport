cc70c6bfb599f72be149f64dab4a5808124d261e
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-08 23:44:34 +0800

    Update NBSoundTypes.java

--- src/main/java/com/unseen/nb/init/NBSoundTypes.java
+++ src/main/java/com/unseen/nb/init/NBSoundTypes.java
@@ -7,42 +7,20 @@ import net.minecraftforge.fml.common.Mod;
 
 public class NBSoundTypes {
 
-    public static final SoundType NYLIUM = new SoundType(1, 1, ModSoundHandler.NYLIUM_BREAK, ModSoundHandler.NYLIUM_STEP,
-            ModSoundHandler.NYLIUM_PLACE, ModSoundHandler.NYLIUM_BLOCK_HIT, ModSoundHandler.NYLIUM_FALL);
+    // ===== 删除所有自定义音效类型 =====
+    // public static final SoundType NYLIUM = new SoundType(1, 1, ModSoundHandler.NYLIUM_BREAK, ModSoundHandler.NYLIUM_STEP, ...);
+    // public static final SoundType HYPHAE = ...;
+    // public static final SoundType CHAIN = ...;
+    // public static final SoundType STEM_TYPE = ...;
+    // public static final SoundType LANTERN = ...;
+    // public static final SoundType WART = ...;
+    // public static final SoundType SOUL_SOIL = ...;
+    // public static final SoundType BASALT = ...;
+    // public static final SoundType NETHER_ORE = ...;
+    // public static final SoundType NETHERITE = ...;
+    // public static final SoundType ANCIENT_DEBRIS = ...;
+    // public static final SoundType SHROOM_LIGHT = ...;
 
-    public static final SoundType HYPHAE = new SoundType(1, 1, ModSoundHandler.HYPHAE_BREAK, ModSoundHandler.HYPHAE_BREAK,
-            ModSoundHandler.HYPHAE_BREAK, ModSoundHandler.HYPHAE_BREAK, ModSoundHandler.HYPHAE_BREAK);
-
-    public static final SoundType CHAIN = new SoundType(1, 1, ModSoundHandler.CHAIN_BREAK, ModSoundHandler.CHAIN_STEP,
-            ModSoundHandler.CHAIN_STEP, ModSoundHandler.CHAIN_BREAK, ModSoundHandler.CHAIN_STEP);
-
-    public static final SoundType STEM_TYPE = new SoundType(1, 1, ModSoundHandler.STEM_BREAK, ModSoundHandler.STEM_STEP,
-            ModSoundHandler.STEM_STEP, ModSoundHandler.STEM_BREAK, ModSoundHandler.STEM_STEP);
-
-    public static final SoundType LANTERN = new SoundType(1, 1, ModSoundHandler.SOUL_LANTERN_BREAK, ModSoundHandler.SOUL_LANTERN_PlACE,
-            ModSoundHandler.SOUL_LANTERN_PlACE, ModSoundHandler.SOUL_LANTERN_BREAK, ModSoundHandler.SOUL_LANTERN_PlACE);
-    public static final SoundType WART = new SoundType(1, 1, ModSoundHandler.WART_BREAK, ModSoundHandler.WART_STEP,
-            ModSoundHandler.WART_PLACE, ModSoundHandler.WART_BLOCK_HIT, ModSoundHandler.WART_PLACE);
-
-    public static final SoundType SOUL_SOIL = new SoundType(1, 1, ModSoundHandler.SOUL_SOIL_BREAK, ModSoundHandler.SOUL_SOIL_STEP,
-            ModSoundHandler.SOUL_SOIL_PLACE, ModSoundHandler.SOUL_SOIL_BLOCK_HIT, ModSoundHandler.SOUL_SOIL_PLACE);
-
-    public static final SoundType BASALT = new SoundType(1, 1, ModSoundHandler.BASALT_BREAK, ModSoundHandler.BASALT_STEP,
-            ModSoundHandler.BASALT_PlACE, ModSoundHandler.BASALT_BLOCK_HIT, ModSoundHandler.BASALT_PlACE);
-
-    public static final SoundType NETHER_ORE = new SoundType(1, 1, ModSoundHandler.NETHER_ORE_BREAK, ModSoundHandler.NETHER_ORE_STEP,
-            ModSoundHandler.NETHER_ORE_PLACE, ModSoundHandler.NETHER_ORE_BLOCK_HIT, ModSoundHandler.NETHER_ORE_PLACE);
-
-    public static final SoundType NETHERITE = new SoundType(1, 1, ModSoundHandler.NETHERITE_BREAK, ModSoundHandler.NETHERITE_STEP,
-            ModSoundHandler.NETHERITE_PLACE, ModSoundHandler.NETHERITE_BLOCK_HIT, ModSoundHandler.NETHERITE_PLACE);
-
-    public static final SoundType ANCIENT_DEBRIS = new SoundType(1, 1, ModSoundHandler.ANCIENT_DEBRIS_BREAK, ModSoundHandler.ANCIENT_DEBRIS_BREAK,
-            ModSoundHandler.ANCIENT_DEBRIS_BREAK, ModSoundHandler.ANCIENT_DEBRIS_BREAK, ModSoundHandler.ANCIENT_DEBRIS_BREAK);
-
-    public static final SoundType SHROOM_LIGHT = new SoundType(1, 1, ModSoundHandler.SHROOM_LIGHT_BREAK, ModSoundHandler.SHROOM_LIGHT_STEP,
-            ModSoundHandler.SHROOM_LIGHT_BREAK, ModSoundHandler.SHROOM_LIGHT_HIT, ModSoundHandler.SHROOM_LIGHT_PlACE);
-
-    
-
-    
+    // ===== 保留 =====
+    // 如果没有任何自定义音效类型被其他代码使用，这个类可以完全删除
 }
