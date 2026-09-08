610343981def374bed8207e130e31a6d475d6bed
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-09 02:43:09 +0800

    Update NBSoundTypes.java

--- src/main/java/com/unseen/nb/init/NBSoundTypes.java
+++ src/main/java/com/unseen/nb/init/NBSoundTypes.java
@@ -12,13 +12,13 @@ public class NBSoundTypes {
             ModSoundHandler.CHAIN_BREAK, 
             ModSoundHandler.CHAIN_STEP);
 
-    // ===== 下界金矿石音效类型 =====
-    public static final SoundType NETHER_ORE = new SoundType(1, 1, 
-            ModSoundHandler.NETHER_ORE_BREAK, 
-            ModSoundHandler.NETHER_ORE_STEP,
-            ModSoundHandler.NETHER_ORE_PLACE, 
-            ModSoundHandler.NETHER_ORE_BLOCK_HIT, 
-            ModSoundHandler.NETHER_ORE_PLACE);
+    // ===== 删除下界金矿石音效类型 =====
+    // public static final SoundType NETHER_ORE = new SoundType(1, 1, 
+    //         ModSoundHandler.NETHER_ORE_BREAK, 
+    //         ModSoundHandler.NETHER_ORE_STEP,
+    //         ModSoundHandler.NETHER_ORE_PLACE, 
+    //         ModSoundHandler.NETHER_ORE_BLOCK_HIT, 
+    //         ModSoundHandler.NETHER_ORE_PLACE);
 
     // ===== 菌光体音效类型 =====
     public static final SoundType SHROOM_LIGHT = new SoundType(1, 1, 
@@ -51,12 +51,4 @@ public class NBSoundTypes {
             ModSoundHandler.ANCIENT_DEBRIS_BREAK, 
             ModSoundHandler.ANCIENT_DEBRIS_BREAK, 
             ModSoundHandler.ANCIENT_DEBRIS_BREAK);
-
-    // ===== 被删除的方块使用的音效类型（已注释） =====
-    // public static final SoundType NYLIUM = ...;
-    // public static final SoundType HYPHAE = ...;
-    // public static final SoundType STEM_TYPE = ...;
-    // public static final SoundType WART = ...;
-    // public static final SoundType SOUL_SOIL = ...;
-    // public static final SoundType BASALT = ...;
 }
