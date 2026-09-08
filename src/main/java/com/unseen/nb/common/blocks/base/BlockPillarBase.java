104f20df3a2540d4a8eb900fbf404da559849a08
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-09 07:16:55 +0800

    Clean up imports and item registration
    Removed unused import and item registration for ModItems.

--- src/main/java/com/unseen/nb/common/blocks/base/BlockPillarBase.java
+++ src/main/java/com/unseen/nb/common/blocks/base/BlockPillarBase.java
@@ -3,7 +3,7 @@ package com.unseen.nb.common.blocks.base;
 import com.unseen.nb.Main;
 import com.unseen.nb.handler.IHasModel;
 import com.unseen.nb.init.ModBlocks;
-import com.unseen.nb.init.ModItems;
+// 删除 import com.unseen.nb.init.ModItems;
 import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -20,9 +20,9 @@ public class BlockPillarBase extends BlockRotatedPillar implements IHasModel {
         setResistance(resistance);
         setSoundType(soundType);
         this.setDefaultState(this.blockState.getBaseState().withProperty(AXIS, EnumFacing.Axis.Y));
-        // Add both an item as a block and the block itself
+        
         ModBlocks.BLOCKS.add(this);
-        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+        // 删除这行：ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
 
     @Override
