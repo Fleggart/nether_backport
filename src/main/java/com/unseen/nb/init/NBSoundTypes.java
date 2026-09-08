3615dae9b3c0de21d43b918dab42a39178783167
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-09 03:00:39 +0800

    Update NBSoundTypes.java

--- src/main/java/com/unseen/nb/init/NBSoundTypes.java
+++ src/main/java/com/unseen/nb/init/NBSoundTypes.java
@@ -12,22 +12,6 @@ public class NBSoundTypes {
             ModSoundHandler.CHAIN_BREAK, 
             ModSoundHandler.CHAIN_STEP);
 
-    // ===== 删除下界金矿石音效类型 =====
-    // public static final SoundType NETHER_ORE = new SoundType(1, 1, 
-    //         ModSoundHandler.NETHER_ORE_BREAK, 
-    //         ModSoundHandler.NETHER_ORE_STEP,
-    //         ModSoundHandler.NETHER_ORE_PLACE, 
-    //         ModSoundHandler.NETHER_ORE_BLOCK_HIT, 
-    //         ModSoundHandler.NETHER_ORE_PLACE);
-
-    // ===== 菌光体音效类型 =====
-    public static final SoundType SHROOM_LIGHT = new SoundType(1, 1, 
-            ModSoundHandler.SHROOM_LIGHT_BREAK, 
-            ModSoundHandler.SHROOM_LIGHT_STEP,
-            ModSoundHandler.SHROOM_LIGHT_BREAK, 
-            ModSoundHandler.SHROOM_LIGHT_HIT, 
-            ModSoundHandler.SHROOM_LIGHT_PlACE);
-
     // ===== 灵魂灯笼音效类型 =====
     public static final SoundType LANTERN = new SoundType(1, 1, 
             ModSoundHandler.SOUL_LANTERN_BREAK, 
