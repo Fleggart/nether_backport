6db29f1a09f210946c1ed8569a6faa5a5cbcd62d
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-09 04:18:27 +0800

    Update CommonProxy.java

--- src/main/java/com/unseen/nb/proxy/CommonProxy.java
+++ src/main/java/com/unseen/nb/proxy/CommonProxy.java
@@ -1,19 +1,10 @@
 package com.unseen.nb.proxy;
 
-// 删除：import com.unseen.nb.common.network.ParticleMessage;
-// 删除：import com.unseen.nb.init.ModNetworkPackets;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
 
 public class CommonProxy {
-
-    public void init() {
-        // 移除事件注册
-        // MinecraftForge.EVENT_BUS.register(new EventOnSoulFire());
-    }
 
     public void registerItemRenderer(Item item, int meta, String id) {}
 
@@ -32,7 +23,6 @@ public class CommonProxy {
         {
             spawnParticle(particleId, posX, posY, posZ, speedX, speedY, speedZ, parameters);
         }
-        // 删除服务端的网络发送逻辑
     }
 
     /** 此方法在 ClientProxy 中重写 */
