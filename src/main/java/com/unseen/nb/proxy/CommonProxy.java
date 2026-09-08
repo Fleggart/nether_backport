69494a4717d63da5cdad7e705b675d957ee20a66
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-09 07:04:33 +0800

    Update CommonProxy.java

--- src/main/java/com/unseen/nb/proxy/CommonProxy.java
+++ src/main/java/com/unseen/nb/proxy/CommonProxy.java
@@ -1,10 +1,36 @@
-// CommonProxy - 只保留签名，不做任何事
-public void spawnParticle(int particleId, double posX, double posY, double posZ, 
-                          double speedX, double speedY, double speedZ, int... parameters) {}
+package com.unseen.nb.proxy;
 
-public void spawnParticle(int particleId, World world, double posX, double posY, double posZ, 
-                          double speedX, double speedY, double speedZ, int... parameters) {
-    if (world.isRemote) {
-        // 这里应该调用 ClientProxy 的实现，但由于代理模式限制，用另一方式
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+
+public class CommonProxy {
+
+    public void init() {
+        // 移除事件注册
+    }
+
+    public void registerItemRenderer(Item item, int meta, String id) {}
+
+    public void registerItemRenderer(Item item, int meta, String id, ResourceLocation resourceOverride) {}
+
+    public void handleAnimationPacket(int entityId, int index) {
+
+    }
+
+    /** 处理粒子生成 - 仅客户端，服务端空实现 */
+    public void spawnParticle(int particleId, double posX, double posY, double posZ, 
+                              double speedX, double speedY, double speedZ, int... parameters) {
+        // 服务端不做任何事情
+    }
+
+    /** 处理粒子生成 - 带World参数，用于客户端检查 */
+    public void spawnParticle(int particleId, World world, double posX, double posY, double posZ, 
+                              double speedX, double speedY, double speedZ, int... parameters) {
+        // 如果是在客户端，调用客户端的方法
+        if (world.isRemote) {
+            spawnParticle(particleId, posX, posY, posZ, speedX, speedY, speedZ, parameters);
+        }
+        // 服务端不做任何事情
     }
 }
