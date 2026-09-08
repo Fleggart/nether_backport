e5a37da2ed89b0aeb21a4989ce97a8ad779cc36f
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-09 05:21:30 +0800

    Update NBSoundTypes.java

--- src/main/java/com/unseen/nb/init/NBSoundTypes.java
+++ src/main/java/com/unseen/nb/init/NBSoundTypes.java
@@ -15,24 +15,8 @@ public class NBSoundTypes {
     // ===== 灵魂灯笼音效类型 =====
     public static final SoundType LANTERN = new SoundType(1, 1, 
             ModSoundHandler.SOUL_LANTERN_BREAK, 
-            ModSoundHandler.SOUL_LANTERN_PlACE,
-            ModSoundHandler.SOUL_LANTERN_PlACE, 
+            ModSoundHandler.SOUL_LANTERN_PLACE,
+            ModSoundHandler.SOUL_LANTERN_PLACE, 
             ModSoundHandler.SOUL_LANTERN_BREAK, 
-            ModSoundHandler.SOUL_LANTERN_PlACE);
-
-    // ===== 下界合金音效类型 =====
-    public static final SoundType NETHERITE = new SoundType(1, 1, 
-            ModSoundHandler.NETHERITE_BREAK, 
-            ModSoundHandler.NETHERITE_STEP,
-            ModSoundHandler.NETHERITE_PLACE, 
-            ModSoundHandler.NETHERITE_BLOCK_HIT, 
-            ModSoundHandler.NETHERITE_PLACE);
-
-    // ===== 远古残骸音效类型 =====
-    public static final SoundType ANCIENT_DEBRIS = new SoundType(1, 1, 
-            ModSoundHandler.ANCIENT_DEBRIS_BREAK, 
-            ModSoundHandler.ANCIENT_DEBRIS_BREAK,
-            ModSoundHandler.ANCIENT_DEBRIS_BREAK, 
-            ModSoundHandler.ANCIENT_DEBRIS_BREAK, 
-            ModSoundHandler.ANCIENT_DEBRIS_BREAK);
+            ModSoundHandler.SOUL_LANTERN_PLACE);
 }
