545be932451ce86507855bef024caaa8647e83bd
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-08 23:01:03 +0800

    Update CommonProxy.java

--- src/main/java/com/unseen/nb/proxy/CommonProxy.java
+++ src/main/java/com/unseen/nb/proxy/CommonProxy.java
@@ -1,6 +1,5 @@
 package com.unseen.nb.proxy;
 
-import com.unseen.nb.common.event.EventOnSoulFire;
 import com.unseen.nb.common.network.ParticleMessage;
 import com.unseen.nb.init.ModNetworkPackets;
 import net.minecraft.item.Item;
@@ -12,10 +11,9 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 public class CommonProxy {
 
     public void init() {
-        MinecraftForge.EVENT_BUS.register(new EventOnSoulFire());
+        // 移除事件注册
+        // MinecraftForge.EVENT_BUS.register(new EventOnSoulFire());
     }
-
-
 
     public void registerItemRenderer(Item item, int meta, String id) {}
 
