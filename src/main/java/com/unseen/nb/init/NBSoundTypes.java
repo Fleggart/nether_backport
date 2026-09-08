2305adb730b5a82304ef15c617c071bb03a4a0cd
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-09 00:05:47 +0800

    Update NBSoundTypes.java

--- src/main/java/com/unseen/nb/init/NBSoundTypes.java
+++ src/main/java/com/unseen/nb/init/NBSoundTypes.java
@@ -7,20 +7,29 @@ import net.minecraftforge.fml.common.Mod;
 
 public class NBSoundTypes {
 
-    // ===== 删除所有自定义音效类型 =====
-    // public static final SoundType NYLIUM = new SoundType(1, 1, ModSoundHandler.NYLIUM_BREAK, ModSoundHandler.NYLIUM_STEP, ...);
+    // ===== 保留所有方块使用的音效类型 =====
+    
+    public static final SoundType NETHER_ORE = new SoundType(1, 1, ModSoundHandler.NETHER_ORE_BREAK, ModSoundHandler.NETHER_ORE_STEP,
+            ModSoundHandler.NETHER_ORE_PLACE, ModSoundHandler.NETHER_ORE_BLOCK_HIT, ModSoundHandler.NETHER_ORE_PLACE);
+
+    public static final SoundType SHROOM_LIGHT = new SoundType(1, 1, ModSoundHandler.SHROOM_LIGHT_BREAK, ModSoundHandler.SHROOM_LIGHT_STEP,
+            ModSoundHandler.SHROOM_LIGHT_BREAK, ModSoundHandler.SHROOM_LIGHT_HIT, ModSoundHandler.SHROOM_LIGHT_PlACE);
+
+    public static final SoundType LANTERN = new SoundType(1, 1, ModSoundHandler.SOUL_LANTERN_BREAK, ModSoundHandler.SOUL_LANTERN_PlACE,
+            ModSoundHandler.SOUL_LANTERN_PlACE, ModSoundHandler.SOUL_LANTERN_BREAK, ModSoundHandler.SOUL_LANTERN_PlACE);
+
+    public static final SoundType NETHERITE = new SoundType(1, 1, ModSoundHandler.NETHERITE_BREAK, ModSoundHandler.NETHERITE_STEP,
+            ModSoundHandler.NETHERITE_PLACE, ModSoundHandler.NETHERITE_BLOCK_HIT, ModSoundHandler.NETHERITE_PLACE);
+
+    public static final SoundType ANCIENT_DEBRIS = new SoundType(1, 1, ModSoundHandler.ANCIENT_DEBRIS_BREAK, ModSoundHandler.ANCIENT_DEBRIS_BREAK,
+            ModSoundHandler.ANCIENT_DEBRIS_BREAK, ModSoundHandler.ANCIENT_DEBRIS_BREAK, ModSoundHandler.ANCIENT_DEBRIS_BREAK);
+
+    // ===== 以下是被删除的方块使用的音效类型（已删除） =====
+    // public static final SoundType NYLIUM = ...;
     // public static final SoundType HYPHAE = ...;
     // public static final SoundType CHAIN = ...;
     // public static final SoundType STEM_TYPE = ...;
-    // public static final SoundType LANTERN = ...;
     // public static final SoundType WART = ...;
     // public static final SoundType SOUL_SOIL = ...;
     // public static final SoundType BASALT = ...;
-    // public static final SoundType NETHER_ORE = ...;
-    // public static final SoundType NETHERITE = ...;
-    // public static final SoundType ANCIENT_DEBRIS = ...;
-    // public static final SoundType SHROOM_LIGHT = ...;
-
-    // ===== 保留 =====
-    // 如果没有任何自定义音效类型被其他代码使用，这个类可以完全删除
 }
