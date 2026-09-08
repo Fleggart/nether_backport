dec7538e3796a7dbb33aad96c002d534be1b8d0d
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-09 07:16:22 +0800

    Update BlockBase.java

--- src/main/java/com/unseen/nb/common/blocks/base/BlockBase.java
+++ src/main/java/com/unseen/nb/common/blocks/base/BlockBase.java
@@ -3,7 +3,7 @@ package com.unseen.nb.common.blocks.base;
 import com.unseen.nb.Main;
 import com.unseen.nb.handler.IHasModel;
 import com.unseen.nb.init.ModBlocks;
-import com.unseen.nb.init.ModItems;
+// 删除 import com.unseen.nb.init.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -16,9 +16,8 @@ public class BlockBase extends Block implements IHasModel {
         setTranslationKey(name);
         setRegistryName(name);
 
-        // Add both an item as a block and the block itself
         ModBlocks.BLOCKS.add(this);
-        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+        // 删除这行：ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
 
     public BlockBase(String name, Material material, float hardness, float resistance, SoundType soundType) {
@@ -28,9 +27,7 @@ public class BlockBase extends Block implements IHasModel {
         setSoundType(soundType);
     }
 
-    /** An easy method for setting up harvesting per individual block that extends this */
-    public BlockBase setHarvestInfo(String tool, int level)
-    {
+    public BlockBase setHarvestInfo(String tool, int level) {
         this.setHarvestLevel(tool, level);
         return this;
     }
